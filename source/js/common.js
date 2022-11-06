0c5d5789b06d528f415a67a0a2a843f897205275
Murad <abishlimurad@gmail.com>
2022-11-06 23:45:12 +0200

    99%

--- source/js/common.js
+++ source/js/common.js
@@ -1,5 +1,11 @@
 const header = document.querySelector('.header'),
-      heroSection = document.querySelector('.hero');
+      heroSection = document.querySelector('.hero'),
+      termsTrigger = document.querySelector('.terms-trigger'),
+      policyTrigger = document.querySelector('.policy-trigger'),
+      termsModal = document.querySelector('#terms-modal'),
+      policyModal = document.querySelector('#policy-modal'),
+      closeTermsModal = document.querySelector('.close-terms'),
+      closePolicyModal = document.querySelector('.close-policy');
 
 function setSpacing() {
   const headerHeight = header.clientHeight;
@@ -7,3 +13,45 @@ function setSpacing() {
 }
 
 setSpacing();
+
+function openModal(currentModal, closeModal) {
+  currentModal.classList.add('--show-modal');
+  closeModal.addEventListener('click', () => {
+    currentModal.classList.remove('--show-modal');
+  })
+}
+
+termsTrigger.addEventListener('click', () => {
+  openModal(termsModal, closeTermsModal);
+})
+
+policyTrigger.addEventListener('click', () => {
+  openModal(policyModal, closePolicyModal);
+})
+
+const burger = document.querySelector('.burger')
+const mobMenu = document.querySelector('.m-menu')
+const body = document.body;
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('_active-burger')
+  if (burger.classList.contains('_active-burger')) {
+    mobMenu.classList.add('--active-menu')
+    body.classList.add('--body-locked')
+  } else {
+    mobMenu.classList.remove('--active-menu')
+    body.classList.remove('--body-locked')
+  }
+})
+
+const menuMobLinks = document.querySelectorAll('.menu-nav__link');
+
+if (menuMobLinks.length > 0) {
+  menuMobLinks.forEach(function (menuMobLinks) {
+    menuMobLinks.addEventListener("click", function (event) {
+      mobMenu.classList.remove('--active-menu');
+      body.classList.remove('--body-locked')
+      burger.classList.remove('_active-burger')
+    });
+  });
+}
